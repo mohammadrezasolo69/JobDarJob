1f41d44a0abb9ac822b5766c949bb4385b94eba9
Mohammadreza <mohammdrezasolo69@gmail.com>
2024-04-28 15:18:34 +0330

    write connection code for connect to mongo

---
+++ db/connection.py
@@ -0,0 +1,10 @@
+from mongoengine import connect
+
+db_name = ""
+host = ""
+port = "" 
+username = ""
+password = ""
+
+
+connect(db_name, host=host, port=port, username=username, password=password)
