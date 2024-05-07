3acf77b768c6774a70611313e68904b9755a514f
Mohammadreza <mohammdrezasolo69@gmail.com>
2024-05-07 15:36:20 +0330

    :memo: change model

--- db/models.py
+++ db/models.py
@@ -1,7 +1,8 @@
+from uuid import uuid5
 from mongoengine import Document,StringField , BooleanField,DateTimeField,UUIDField,ImageField,URLField,DateField,ReferenceField,ListField
 
 class Provider(Document):
-    id = UUIDField()
+    slug = StringField(max_length=100,required=True,unique=True)
     name = StringField(max_length=100,required=True,unique=True)
     is_active = BooleanField(default=True)
     run_date = DateTimeField(required=True)
@@ -9,18 +10,16 @@ class Provider(Document):
 
 
 class JobPosition(Document):
-    id = UUIDField()
-    title = StringField(max_length=300,required=True)
+    title = StringField(max_length=300, required=True)
+    slug = StringField(max_length=100,required=True,unique=True)
     company_city = StringField(max_length=200,required=True)
     company_name = StringField(max_length=200,required=True)
-    cover = ImageField(required=True)
-    url = URLField(required=True)
-    date = DateField(required=True)
-    provider = ReferenceField(Provider)
+    cover = URLField(required=True)
+    url_detail = URLField(required=True)
+    date = DateField()
     is_active = BooleanField(default=True)
-    expired_date = DateField(required=True)
-    category = ListField(required=False)
     type_cooperation = StringField(max_length=100,required=False)
+    provider = ReferenceField(Provider)
 
 
     def job_position_count(self):
